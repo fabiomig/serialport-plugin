d17f4173c0bac73e66d746484d959a253fa9b2e0
Fábio Oliveira <fabioniguel3@gmail.com>
2015-07-09 17:05:32 +0100

    34

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -23,13 +23,13 @@ public class Hello extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
-        File file = new File ("/dev/ttyS2");
+        File file = new File ("/dev/","ttyS2");
 
         try {
             serialPort = new SerialPort(file, 115200, 1);
             mOutputStream = serialPort.getOutputStream();
             mInputStream  = serialPort.getInputStream();
-            mOutputStream.write(new String("Teste").getBytes());
+            //mOutputStream.write(new String("Teste").getBytes());
             mOutputStream.write('\n');
 
         } catch (IOException ex) {
