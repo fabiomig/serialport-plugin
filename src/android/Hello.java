1a4b5d6abe2456081bbf1e3ed701a3f193778866
Fábio Oliveira <fabioniguel3@gmail.com>
2015-07-13 11:20:20 +0100

    Update Hello.java

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -47,7 +47,7 @@ public class Hello extends CordovaPlugin {
 
             try {
                 
-                this.open_con(new String(message).getBytes());
+                this.open_con(message);
                 
             } catch (IOException ex) {
                 
