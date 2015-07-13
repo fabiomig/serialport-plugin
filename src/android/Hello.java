f9970c51ca904557b67cf7d74f4a682975b2b26c
Fábio Oliveira <fabioniguel3@gmail.com>
2015-07-13 10:58:56 +0100

    Update Hello.java

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -30,6 +30,8 @@ public class Hello extends CordovaPlugin {
                 //mInputStream  = serialPort.getInputStream();
                 //mOutputStream.write(new String(text).getBytes());
                 mOutputStream.write(new String(message).getBytes());
+                mSerialPort.close();
+                mSerialPort = null;
         } catch (IOException ex) {
                 ex.printStackTrace();
                 return false;
